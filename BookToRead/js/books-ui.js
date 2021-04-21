e433efc29e0311bf555559da95e29041907315c3
Laritcheff <68246179+Laritcheff@users.noreply.github.com>
2021-04-21 13:03:32 +0300

    step2

--- BookToRead/js/books-ui.js
+++ BookToRead/js/books-ui.js
@@ -1,34 +1,59 @@
 export class BooksUI {
   searchResultHolder;
+  bookInfoHolder;
+  currentPage = [];
+
   api;
+
   constructor(api) {
     this.searchResultHolder = document.getElementById("searchResultHolder");
-   
+
     const bookInfoHolder = document.getElementById("bookInfoHolder");
-    console.log(bookInfoHolder);
     const searchInput = document.getElementById("searchInput");
     const searchBtn = document.getElementById("searchBtn");
 
     searchBtn.addEventListener("click", (event) => {
       event.preventDefault();
       const querry = searchInput.value;
-      console.log(querry);
       if (!querry) {
-        return;
+        alert();
       }
 
       api.search(querry).then((page) => {
         this.processSearchResult(page);
       });
     });
-    this.searchResultHolder.addEventListener("click", (event) =>
-      console.log(event.target)
-    );
+
+    this.searchResultHolder.addEventListener("click", (event) => {
+      const targetDiv = event.target;
+      const id = targetDiv.id;
+      const selectedBook = this.currentPage.find((item) => item.id === id);
+      if (!selectedBook) {
+        return;
+      }
+
+      if (this.selectedBook) {
+        const selectedBook = this.searchResultHolder.querySelector(
+          "#" + this.selectedBook.id
+        );
+        selectedBook.classList.remove("selected-item");
+      }
+
+      this.selectedBook = selectedBook;
+      targetDiv.classList.add("selected-item");
+
+      localStorage.setItem(id, selectedBook.author_name[0]);
+      console.log(selectedBook.author_name[0]);
+
+      bookInfoHolder.innerHTML = selectedBook.author_name[0];
+    });
   }
+
   processSearchResult(page) {
     page.docs.forEach((item) => {
       item.id = item.key.split("/").pop();
     });
+    this.currentPage = page.docs;
 
     const bookHTML = page.docs.reduce((acc, item) => {
       return (
